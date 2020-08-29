5fc84371e140dd97cb7feb93bf6e24bbae8f160a
YsarocK <63562160+YsarocK@users.noreply.github.com>
2020-08-29 17:56:17 +0200

    Vuetify is missing in Windows App

--- jarvis-app/nuxt.config.js
+++ jarvis-app/nuxt.config.js
@@ -1,6 +1,11 @@
 module.exports = {
 	mode: 'spa',
-	head: {title: 'jarvis-electron'}, // Headers of the page
+	head: {title: 'Jarvis APP',
+			link: [
+				{ rel: 'icon', type: 'image/jpg', href: '/logo.jpg' },
+				{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Signika:wght@300;400;500;600;700&display=swap' }
+			]
+	}, // Headers of the page
 	loading: false, // Disable default loading bar
 	build: {
 		extend (config, { isDev, isClient }) {
